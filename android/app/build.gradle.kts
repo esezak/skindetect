f55bb32f7fe08be08dc1a9f4c739a63595247ba5
Ege Sezak <egesezaki03@gmail.com>
2026-01-07 23:18:59 +0300

    initialize Flutter project structure with essential files and configurations

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.esezak.skindetect"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,9 +29,11 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Use debug signing keys so `flutter run --release` works locally
             signingConfig = signingConfigs.getByName("debug")
+            // Optionally enable shrinking for store builds
+            // isMinifyEnabled = true
+            // proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
